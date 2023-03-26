13a93ce4b4f08232b905041d4bcba16a40fe5a66
Dag Stuan <dag.stuan@bekk.no>
2023-03-26 20:15:28 +0200

    Use memory cache for requests to Varsom API

--- SkredvarselGarminWeb/SkredvarselGarminWeb/Endpoints/VarsomApiRouteBuilderExtensions.cs
+++ SkredvarselGarminWeb/SkredvarselGarminWeb/Endpoints/VarsomApiRouteBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Caching.Memory;
 using SkredvarselGarminWeb.Endpoints.Mappers;
 using SkredvarselGarminWeb.Endpoints.Models;
 using SkredvarselGarminWeb.VarsomApi;
@@ -8,25 +9,50 @@ public static class VarsomApiRouteBuilderExtensions
 {
     public static void MapVarsomApiEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/simpleWarningsByRegion/{regionId}/{langKey}/{from}/{to}", async (string regionId, string langKey, DateOnly from, DateOnly to, IVarsomApi varsomApi) =>
+        app.MapGet("/api/simpleWarningsByRegion/{regionId}/{langKey}/{from}/{to}", async (
+            string regionId,
+            string langKey,
+            DateOnly from,
+            DateOnly to,
+            IVarsomApi varsomApi,
+            IMemoryCache memoryCache) =>
         {
-            var warnings = await varsomApi.GetWarningsByRegion(regionId, langKey, from, to);
-
-            return warnings.Select(w => new SimpleAvalancheWarning
+            var cacheKey = $"SimpleWarnings_{regionId}_{langKey}_{from:yyyy-MM-dd}_{to:yyyy-MM-dd}";
+            return await memoryCache.GetOrCreateAsync(cacheKey, async (cacheEntry) =>
             {
-                DangerLevel = int.Parse(w.DangerLevel),
-                Validity = new DateTime[] {
-                    w.ValidFrom,
-                    w.ValidTo
-                }
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+
+                var warnings = await varsomApi.GetWarningsByRegion(regionId, langKey, from, to);
+
+                return warnings.Select(w => new SimpleAvalancheWarning
+                {
+                    DangerLevel = int.Parse(w.DangerLevel),
+                    Validity = new DateTime[] {
+                        w.ValidFrom,
+                        w.ValidTo
+                    }
+                });
             });
         }).RequireAuthorization("Garmin");
 
-        app.MapGet("/api/detailedWarningsByRegion/{regionId}/{langKey}/{from}/{to}", async (string regionId, string langKey, DateOnly from, DateOnly to, IVarsomApi varsomApi) =>
+        app.MapGet("/api/detailedWarningsByRegion/{regionId}/{langKey}/{from}/{to}", async (
+            string regionId,
+            string langKey,
+            DateOnly from,
+            DateOnly to,
+            IVarsomApi varsomApi,
+            IMemoryCache memoryCache) =>
         {
-            var warnings = await varsomApi.GetDetailedWarningsByRegion(regionId, langKey, from, to);
+            var cacheKey = $"DetailedWarnings_{regionId}_{langKey}_{from:yyyy-MM-dd}_{to:yyyy-MM-dd}";
 
-            return warnings.Select(w => w.ToDetailedAvalancheWarning());
+            return await memoryCache.GetOrCreateAsync(cacheKey, async (cacheEntry) =>
+            {
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+
+                var warnings = await varsomApi.GetDetailedWarningsByRegion(regionId, langKey, from, to);
+
+                return warnings.Select(w => w.ToDetailedAvalancheWarning());
+            });
         }).RequireAuthorization("Garmin");
     }
 }
