4b74ccc55759a3ca865c41bbc03f39ee660d4b20
Dag Stuan <dag.stuan@bekk.no>
2023-11-21 22:09:01 +0100

    Upgrade to .NET 8

--- SkredvarselGarminWeb/SkredvarselGarminWeb/Configuration/GarminAuthenticationHandler.cs
+++ SkredvarselGarminWeb/SkredvarselGarminWeb/Configuration/GarminAuthenticationHandler.cs
@@ -13,8 +13,7 @@ public partial class GarminAuthenticationHandler : AuthenticationHandler<GarminA
     public GarminAuthenticationHandler(
         IOptionsMonitor<GarminAuthenticationSchemeOptions> options,
         ILoggerFactory logger,
-        UrlEncoder encoder,
-        ISystemClock clock) : base(options, logger, encoder, clock)
+        UrlEncoder encoder) : base(options, logger, encoder)
     {
     }
 
