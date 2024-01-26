16dcfe343939e881a3a319eb81ad8934b3f46279
Dag Stuan <dag.stuan@bekk.no>
2024-01-26 23:07:21 +0100

    Migration for restricting deletions.

--- SkredvarselGarminWeb/SkredvarselGarminWeb/Migrations/20240126220648_RestrictDeletions.Designer.cs
+++ SkredvarselGarminWeb/SkredvarselGarminWeb/Migrations/20240126220648_RestrictDeletions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SkredvarselGarminWeb.Database;
@@ -11,9 +12,11 @@ using SkredvarselGarminWeb.Database;
 namespace SkredvarselGarminWeb.Migrations
 {
     [DbContext(typeof(SkredvarselDbContext))]
-    partial class SkredvarselDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240126220648_RestrictDeletions")]
+    partial class RestrictDeletions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SkredvarselGarminWeb/SkredvarselGarminWeb/Migrations/20240126220648_RestrictDeletions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkredvarselGarminWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestrictDeletions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_agreements_users_user_id",
+                table: "agreements");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_stripe_subscriptions_users_user_id",
+                table: "stripe_subscriptions");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_agreements_users_user_id",
+                table: "agreements",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_stripe_subscriptions_users_user_id",
+                table: "stripe_subscriptions",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_agreements_users_user_id",
+                table: "agreements");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_stripe_subscriptions_users_user_id",
+                table: "stripe_subscriptions");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_agreements_users_user_id",
+                table: "agreements",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_stripe_subscriptions_users_user_id",
+                table: "stripe_subscriptions",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
