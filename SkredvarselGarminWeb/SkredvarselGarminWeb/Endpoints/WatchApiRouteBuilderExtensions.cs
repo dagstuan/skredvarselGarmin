6a78671c173049fe61d4957151e1accf8eca5d8e
Dag Stuan <dag.stuan@bekk.no>
2024-12-05 22:43:21 +0100

    Upgrade to .net 9

--- SkredvarselGarminWeb/SkredvarselGarminWeb/Endpoints/WatchApiRouteBuilderExtensions.cs
+++ SkredvarselGarminWeb/SkredvarselGarminWeb/Endpoints/WatchApiRouteBuilderExtensions.cs
@@ -74,7 +74,7 @@ public static class WatchApiRouteBuilderExtensions
             [FromHeader(Name = "Authorization")] string authorizationHeader,
             IGarminAuthenticationService garminAuthenticationService) =>
         {
-            if (authorizationHeader.IsNullOrEmpty())
+            if (string.IsNullOrEmpty(authorizationHeader))
             {
                 return Results.Unauthorized();
             }
